f551f437836ea7b74da64d43cf91739b6afaed72
xresco <xresco@gmail.com>
2014-11-12 21:07:29 +0800

    few tweaks to make for the sample app

--- app/build/generated/source/r/debug/com/robox/circlelayoutexample/R.java
+++ app/build/generated/source/r/debug/com/robox/circlelayoutexample/R.java
@@ -94,11 +94,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int circularLayout=0x7f040007;
         public static final int normal=0x7f040000;
         public static final int pie=0x7f040001;
+        public static final int skChildCount=0x7f040003;
+        public static final int skOffsetX=0x7f040004;
+        public static final int skOffsetY=0x7f040005;
+        public static final int skRad=0x7f040002;
         public static final int tglBtn=0x7f040006;
-        public static final int txtChildCount=0x7f040002;
-        public static final int txtOffsetX=0x7f040003;
-        public static final int txtOffsetY=0x7f040004;
-        public static final int txtRad=0x7f040005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- app/src/main/java/com/robox/circlelayoutexample/MainActivity.java
+++ app/src/main/java/com/robox/circlelayoutexample/MainActivity.java
@@ -7,58 +7,72 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener{
+    int progressChanged = 0;
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        progressChanged = i;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+        if(seekBar.getId()==R.id.skRad) {
+            circularLayout.setRadius(progressChanged);
+            circularLayout.init();
+        }
+        if(seekBar.getId()==R.id.skChildCount) {
+            circularLayout.setChildrenCount(progressChanged);
+            circularLayout.init();
+        }
+        if(seekBar.getId()==R.id.skOffsetX) {
+            circularLayout.setOffsetX(progressChanged-90);
+            circularLayout.init();
+        }
+        if(seekBar.getId()==R.id.skOffsetY) {
+            circularLayout.setOffsetY(progressChanged-60);
+            circularLayout.init();
+        }
+    }
+
     CircleLayout circularLayout ;
+
     public void tglClick(View v)
     {
         ToggleButton tg=(ToggleButton)findViewById(R.id.tglBtn);
         circularLayout.setChildrenPinned(tg.isChecked());
         circularLayout.init();
     }
-    public void btnClick(View v)
-    {
-        EditText childCount=(EditText)findViewById(R.id.txtChildCount);
-        EditText offsetX=(EditText)findViewById(R.id.txtOffsetX);
-        EditText offsetY=(EditText)findViewById(R.id.txtOffsetY);
-        EditText radius=(EditText)findViewById(R.id.txtRad);
-        try {
-            if (radius.getText() != null) {
-                circularLayout.setRadius((Integer.parseInt(radius.getText().toString())));
-                circularLayout.init();
-            }
-        }
-        catch (Exception e){Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT);}
-        try {
-            if (childCount.getText() != null) {
-                circularLayout.setChildrenCount((Integer.parseInt(childCount.getText().toString())));
-                circularLayout.init();
-            }
-        }
-        catch (Exception e){Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT);}
-        try {
-            if (offsetX.getText() != null) {
-                circularLayout.setOffsetX((Integer.parseInt(offsetX.getText().toString())));
-                circularLayout.init();
-            }
-        }
-        catch (Exception e){Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT);}
-        try {
-            if (offsetY.getText() != null) {
-                circularLayout.setOffsetY((Integer.parseInt(offsetY.getText().toString())));
-                circularLayout.init();
-            }
-        }
-        catch (Exception e){Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT);}
 
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SeekBar skRad = (SeekBar) findViewById(R.id.skRad);
+        skRad.setOnSeekBarChangeListener(this);
+
+        SeekBar skOffsetX = (SeekBar) findViewById(R.id.skOffsetX);
+        skOffsetX.setOnSeekBarChangeListener(this);
+
+        SeekBar skOffsetY = (SeekBar) findViewById(R.id.skOffsetY);
+        skOffsetY.setOnSeekBarChangeListener(this);
+
+        SeekBar skChildCount = (SeekBar) findViewById(R.id.skChildCount);
+        skChildCount.setOnSeekBarChangeListener(this);
+
+
 
         circularLayout =(CircleLayout) findViewById(R.id.circularLayout);
 
